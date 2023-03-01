13c44df3655dd5cdcd36af2fbe0841ed2ca552fc
Misael Alberto <misael.alberto@outlook.com>
2023-03-01 08:45:32 -0300

    inserindo configuração mysql 8

--- config/db.js
+++ config/db.js
@@ -1,23 +1,27 @@
-// const mysql = require('mysql2');
-const mysql = require('mysql');
-connectionString = process.env.DATABASE_URL 
+/* --  Usando mysql 5.3 ---*/
+//const mysql = require('mysql');
+//connectionString = process.env.DATABASE_URL 
 
-//console.log(connectionString);
+/* --  Usando mysql 8 ---*/
+const mysql = require('mysql2');
+
 
 db = {};
 db.cnn = {};
 db.cnn.exec = function(query, callback){
-    // var connection = mysql.createConnection({
-    //     host: process.env.HOST,
-    //     user: process.env.USER,
-    //     password: process.env.PASSWORD,
-    //     database: process.env.DATABASE 
-    // });
-    var connection = mysql.createConnection(connectionString);
-    connection.query(query, function(err, rows){
-        callback(rows, err);
-        connection.end();
+    /* --  Usando mysql 8 ---*/
+    var connection = mysql.createConnection({
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE 
     });
+    /* --  Usando mysql 5 ---*/
+    // var connection = mysql.createConnection(connectionString);
+    // connection.query(query, function(err, rows){
+    //     callback(rows, err);
+    //     connection.end();
+    // });
 };
 
 module.exports = db;
